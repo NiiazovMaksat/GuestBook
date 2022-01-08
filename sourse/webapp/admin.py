8ab326e8029d5e9ed6f6ab5221d8ba6d6082de61
NiiazovMaksat <niyazov37@gmail.com>
2022-01-08 12:34:36 +0600

    добавил Модель и зарегистрировал ее в Адимнку

--- sourse/webapp/admin.py
+++ sourse/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from webapp.models import Book
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'updated_at', 'status']
+    list_filter = ['status']
+    search_fields = ['name', 'email']
+    fields = ['name', 'email', 'updated_at', 'status']
+
+admin.site.register(Book, BookAdmin)
--- sourse/webapp/models.py
+++ sourse/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [('active', 'Активная'), ('blocked', 'Заблокировано')]
+
+class Book(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False, verbose_name='Имя')
+    email = models.EmailField(max_length=50, null=False, blank=False, verbose_name='Email')
+    text = models.TextField(max_length=2000, null=False, blank=True, verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    status = models.CharField(max_length=30, null=False, blank=False, default="active", choices=STATUS_CHOICES, verbose_name='Статус')
+
+    def __str__(self):
+        return f'{self.pk}) {self.name} : {self.email} / {self.updated_at} | {self.status}'
+
+    class Meta:
+        db_table = 'guest_book'
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
