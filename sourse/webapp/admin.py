c18747b3b93c544b2b76a5573750dab9fa6a1f97
NiiazovMaksat <niyazov37@gmail.com>
2022-01-08 12:44:55 +0600

    внесены поправки в Адимнку и добавлена миграция

--- sourse/webapp/admin.py
+++ sourse/webapp/admin.py
@@ -6,6 +6,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'updated_at', 'status']
     list_filter = ['status']
     search_fields = ['name', 'email']
-    fields = ['name', 'email', 'updated_at', 'status']
+    fields = ['name', 'email', 'text', 'created_at', 'updated_at', 'status']
+    readonly_fields = ['created_at', 'updated_at']
 
 admin.site.register(Book, BookAdmin)
